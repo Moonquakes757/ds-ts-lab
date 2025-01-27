5964f1b8dd496eca23e461468bbb2cfca60ae95a
Moonquakes757 <xsh20020902@gmail.com>
2025-01-27 15:09:04 +0000

    Type aliases demonstration.

---
+++ src/03-typeAliases.ts
@@ -0,0 +1,80 @@
+import {
+    ColleagueV2,
+    Friend,
+    Buddy,
+    BuddyList,
+    Administrator,
+  } from "./myTypes";
+  import { friends } from "./01-basics";
+
+  const colleague1: ColleagueV2 = {
+    name: "Ralph Graham",
+    department: "Engineering",
+    contact: {
+      email: "rgraham@company.com",
+      extension: 121,
+    },
+  };
+  
+
+  function makeBuddyList(
+    name: string,
+    buddies: Buddy[],
+    admin?: Administrator
+  ): BuddyList {
+    return {
+      name,
+      members: buddies,
+      administrator: admin,
+    } as BuddyList;
+    // The as operator above casts an object to a specific type.
+  }
+  // Tests for makeBuddyList
+  const myFootballBuddies = makeBuddyList(
+    "Football team",
+    [colleague1, friends[0], colleague1],
+    colleague1
+  )
+  
+  const myBandBuddies = makeBuddyList(
+      "Band name",
+      [colleague1, friends[1]]
+      // No administrator
+    )
+  
+  console.log(myFootballBuddies)
+  console.log(myBandBuddies)
+  //--------------------------------------
+  function findBuddyContact(list: BuddyList, name: string): string | undefined {
+    for (const buddy of list.members) {
+      if (buddy.name === name) {
+        if ("phone" in buddy) {
+          return buddy.phone;
+        }
+        else {
+          return buddy.contact.email;
+        }
+      }
+      return undefined;
+    }
+  }
+  // Test for findBuddyContact.
+  console.log("Contact buddy at: ", findBuddyContact(myFootballBuddies, "Ralph Graham"));
+  
+
+  // Function to get all Friends in a BuddyList
+function getBuddyListFriends(list: BuddyList): Friend[] {
+    return list.members.reduce<Friend[]>((friendsArray, buddy) => {
+      if ("phone" in buddy) {
+        friendsArray.push(buddy as Friend); // Cast buddy to Friend
+      }
+      return friendsArray;
+    }, []);
+  }
+  
+  // Test for getBuddyListFriends
+  const footballFriends = getBuddyListFriends(myFootballBuddies);
+  const bandFriends = getBuddyListFriends(myBandBuddies);
+  
+  console.log("Football Friends: ", footballFriends);
+  console.log("Band Friends: ", bandFriends);
